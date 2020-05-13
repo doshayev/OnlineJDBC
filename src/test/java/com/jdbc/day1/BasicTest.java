40e58a0a02839581ee8256778188469946029025
Adil Doshayev <doshayev@gmail.com>
2020-05-13 14:42:24 -0500

    Basic Test

--- src/test/java/com/jdbc/day1/BasicTest.java
+++ src/test/java/com/jdbc/day1/BasicTest.java
@@ -1,4 +1,20 @@
 package com.jdbc.day1;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class BasicTest {
+    public static void main(String[] args) throws SQLException {
+        String URL = "jdbc:oracle:thin:@3.90.175.72:1521:xe";
+        String username = "hr";
+        String password = "hr";
+        //to establish connection with a database
+
+        Connection connection = DriverManager.getConnection(URL, username, password);
+
+
+
+    }
+
 }
